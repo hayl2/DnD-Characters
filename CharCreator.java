a4a838f87661c56b0eceb58211e9a47d89a0b233
aanonson <s-shia@bsd405.org>
2023-06-06 14:05:47 -0700

    charcreator

--- CharCreator.java
+++ CharCreator.java
@@ -1,7 +1,16 @@
 import java.io.Serializable;
 
-public class CharCreator {
+public class CharCreator implements Serializable {
     // race, class, name
-    System.out.println("Welcome, welcome! In order to proceed, you must first create a character.")
+    private static String name;
 
+    System.out.println("Welcome, welcome! In order to proceed, you must first create a character.");
+    System.out.println("What will your character's name be?");
+    name = scan.nextLine();
+
+}
+
+public Character(String name, String race, String class) {
+    this.race;
+    this.class;
 }
