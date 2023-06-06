028008ec839eb3242b04188f1096272b64bd9faf
aanonson <s-shia@bsd405.org>
2023-06-06 13:53:22 -0700

    meow

---
+++ CharCreator.java
@@ -0,0 +1,7 @@
+import java.io.Serializable;
+
+public class CharCreator {
+    // race, class, name
+    System.out.println("Welcome, welcome! In order to proceed, you must first create a character.")
+
+}
