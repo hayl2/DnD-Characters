f6833293abf62f3bc702d181add5469acdf448b6
aanonson <s-shia@bsd405.org>
2023-06-06 14:29:01 -0700

    charcreator

--- CharCreator.java
+++ CharCreator.java
@@ -1,16 +1,18 @@
 import java.io.Serializable;
+import java.util.Scanner;
 
-public class CharCreator implements Serializable {
-    // race, class, name
+public class CharCreator extends Game {
+    Scanner scan = new.Scanner(System.in);
+
     private static String name;
     private static String race;
-    private static String class;
+    private static String classDND;
     
     System.out.println("What will your character's name be?");
     name = scan.nextLine();
 
     System.out.println("What will your character's class be?");
-    class = scan.nextline();
+    classDND = scan.nextline();
 
-    return "You are " + name + ", a " + race + " in the " + class + " class.";
+    return "You are " + name + ", a " + race + " in the " + classDND + " class.";
 }
